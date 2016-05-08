e13ae5a228c75dd5af586a6a3c40c3346b98b6f4
DamonDeng <dengmingxuan@hotmail.com>
2016-05-08 18:10:23 +0800

    with sh copy to local

---
+++ get_resource.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+aws s3 cp s3://resource.git.sig.damondeng.com/SparkWiki/.gitignore ./.gitignore
+
+aws s3 cp s3://resource.git.sig.damondeng.com/SparkWiki/lib/lucene-core-3.6.0.jar ./lib/lucene-core-3.6.0.jar
+
+aws s3 cp s3://resource.git.sig.damondeng.com/SparkWiki/lib/IKAnalyzer2012_u6.jar ./lib/IKAnalyzer2012_u6.jar
+
